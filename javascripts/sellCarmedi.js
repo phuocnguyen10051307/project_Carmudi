da764525a25186eb442a7336e5e23064deec9860
TruongAnhKietK49 <4901104078@student.hcmue.edu.vn>
2024-03-31 10:59:59 +0700

    project_Carmudi

--- javascripts/sellCarmedi.js
+++ javascripts/sellCarmedi.js
@@ -1,14 +1,14 @@
-var hangXe = document.getElementById("hangxe");
-var values = ["VinFast", "Toyota", "Ford", "BMW"];
+let hangXe = document.getElementById("hangxe");
+let values = ["VinFast", "Toyota", "Ford", "BMW"];
 values.forEach(function (value) {
-  var option = document.createElement("option");
+  let option = document.createElement("option");
   option.text = value;
   option.value = value;
   hangXe.add(option);
 });
 function hXe() {
-  var x = document.getElementById("hangxe").value;
-  var dongXe = document.getElementById("dongxe");
+  let x = document.getElementById("hangxe").value;
+  let dongXe = document.getElementById("dongxe");
   let dXe = [];
   if (x != "Chọn hãng xe") {
     dongXe.disabled = false;
@@ -16,7 +16,7 @@ function hXe() {
       dongXe.innerHTML = " ";
       dXe = ["Fadil", "VF3", "VF4"];
       dXe.forEach(function (value) {
-        var option = document.createElement("option");
+        let option = document.createElement("option");
         option.text = value;
         option.value = value;
         dongXe.add(option);
@@ -26,7 +26,7 @@ function hXe() {
       dongXe.innerHTML = " ";
       dXe = ["Allex", "Allion", "Alphard"];
       dXe.forEach(function (value) {
-        var option = document.createElement("option");
+        let option = document.createElement("option");
         option.text = value;
         option.value = value;
         dongXe.add(option);
@@ -36,7 +36,7 @@ function hXe() {
       dongXe.innerHTML = " ";
       dXe = ["Artic", "Capri", "Cargo"];
       dXe.forEach(function (value) {
-        var option = document.createElement("option");
+        let option = document.createElement("option");
         option.text = value;
         option.value = value;
         dongXe.add(option);
@@ -46,7 +46,7 @@ function hXe() {
       dongXe.innerHTML = " ";
       dXe = ["1600", "1800", "2000"];
       dXe.forEach(function (value) {
-        var option = document.createElement("option");
+        let option = document.createElement("option");
         option.text = value;
         option.value = value;
         dongXe.add(option);
@@ -55,19 +55,71 @@ function hXe() {
   } else dongXe.disabled = true;
 }
 function dXe() {
-  var x = document.getElementById("dongxe").value;
-  var phienBan = document.getElementById("phienban");
+  let x = document.getElementById("dongxe").value;
+  let phienBan = document.getElementById("phienban");
   let pBan = [];
   if (x != "Chọn dòng xe") {
     phienBan.disabled = false;
-    if (x == "Fadil") {
-      pBan = ["Cao cấp", "Nâng cao", "Tiêu chuẩn"];
+    if (x == "Fadil" || x == "VF3" || x == "VF4") {
+      phienBan.innerHTML = [];
+      pBan = ["Cao cấp", "Nâng cao", "Tiêu chuẩn", "Plus"];
       pBan.forEach(function (value) {
-        var option = document.createElement("option");
+        let option = document.createElement("option");
+        option.text = value;
+        option.value = value;
+        phienBan.add(option);
+      });
+    }
+    if (x == "Allex" || x == "Allion" || x == "Alphard") {
+      phienBan.innerHTML = [];
+      pBan = ["Khác", "VT3", "3.0 Sedan (FWD)"];
+      pBan.forEach(function (value) {
+        let option = document.createElement("option");
+        option.text = value;
+        option.value = value;
+        phienBan.add(option);
+      });
+    }
+    if (x == "Artic" || x == "Capri" || x == "Cargo") {
+      phienBan.innerHTML = [];
+      pBan = ["Khác", "1.0 Coupe", "1.4 Van (FWD)"];
+      pBan.forEach(function (value) {
+        let option = document.createElement("option");
+        option.text = value;
+        option.value = value;
+        phienBan.add(option);
+      });
+    }
+    if (x == "1600" || x == "1800" || x == "2000") {
+      phienBan.innerHTML = [];
+      pBan = ["Khác", "VT 300"];
+      pBan.forEach(function (value) {
+        let option = document.createElement("option");
         option.text = value;
         option.value = value;
         phienBan.add(option);
       });
     }
   }
+}
+
+let nsx = document.getElementById("nsx");
+let value = [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024];
+value.forEach(function (value) {
+  let option = document.createElement("option");
+  option.text = value;
+  option.value = value;
+  nsx.add(option);
+});
+
+function button() {
+  let x = document.getElementById("hangxe").value;
+  let y = document.getElementById("dongxe").value;
+  let z = document.getElementById("phienban").value;
+  let t = document.getElementById("kilometer").value;
+  let u = document.getElementById("phone").value;
+  let btn = document.getElementById("btn");
+  if (x != " " && y != " " && z != " " && t != " " && u > 10**9 ) {
+    btn.disabled = false;
+  } else return true;
 }
