0270a87a7892c8f7fd696d1f2e3738770594109e
TruongAnhKietK49 <4901104078@student.hcmue.edu.vn>
2024-04-01 22:47:48 +0700

    updata sellCarmudi

--- javascripts/sellCarmedi.js
+++ javascripts/sellCarmedi.js
@@ -1,125 +1,132 @@
-let hangXe = document.getElementById("hangxe");
+let getCarCompany = document.getElementById("carCompany");
 let values = ["VinFast", "Toyota", "Ford", "BMW"];
 values.forEach(function (value) {
   let option = document.createElement("option");
   option.text = value;
   option.value = value;
-  hangXe.add(option);
+  getCarCompany.add(option);
 });
-function hXe() {
-  let x = document.getElementById("hangxe").value;
-  let dongXe = document.getElementById("dongxe");
-  let dXe = [];
-  if (x != "Chọn hãng xe") {
-    dongXe.disabled = false;
-    if (x == "VinFast") {
-      dongXe.innerHTML = " ";
-      dXe = ["Fadil", "VF3", "VF4"];
-      dXe.forEach(function (value) {
-        let option = document.createElement("option");
-        option.text = value;
-        option.value = value;
-        dongXe.add(option);
-      });
+
+//set option for Vehicles select
+let getVehicles = document.getElementById("vehicles");
+const vinfast = ["Fadil", "VF3", "VF4"];
+const toyota = ["Allex", "Allion", "Alphard"];
+const ford = ["Artic", "Capri", "Cargo"];
+const bmw = ["1600", "1800", "2000"];
+const printfOptionVehicles = (arr) => {
+  // làm rỗng option
+  getVehicles.innerHTML = " ";
+  // create 1 option
+  let firstOption = document.createElement("option");
+  firstOption.text = "Chọn dòng xe";
+  getVehicles.add(firstOption);
+  // push option được chọn
+  arr.forEach(function (value) {
+    let option = document.createElement("option");
+    option.text = value;
+    option.value = value;
+    getVehicles.add(option);
+  });
+};
+function carCompany(value) {
+  if (value != "Chọn hãng xe") {
+    //turn on option
+    getVehicles.disabled = false;
+    switch (value) {
+      case "VinFast":
+        printfOptionVehicles(vinfast);
+        break;
+      case "Toyota":
+        printfOptionVehicles(toyota);
+        break;
+      case "Ford":
+        printfOptionVehicles(ford);
+        break;
+      case "BMW":
+        printfOptionVehicles(bmw);
+        break;
+      default:
+        break;
     }
-    if (x == "Toyota") {
-      dongXe.innerHTML = " ";
-      dXe = ["Allex", "Allion", "Alphard"];
-      dXe.forEach(function (value) {
-        let option = document.createElement("option");
-        option.text = value;
-        option.value = value;
-        dongXe.add(option);
-      });
-    }
-    if (x == "Ford") {
-      dongXe.innerHTML = " ";
-      dXe = ["Artic", "Capri", "Cargo"];
-      dXe.forEach(function (value) {
-        let option = document.createElement("option");
-        option.text = value;
-        option.value = value;
-        dongXe.add(option);
-      });
-    }
-    if (x == "BMW") {
-      dongXe.innerHTML = " ";
-      dXe = ["1600", "1800", "2000"];
-      dXe.forEach(function (value) {
-        let option = document.createElement("option");
-        option.text = value;
-        option.value = value;
-        dongXe.add(option);
-      });
-    }
-  } else dongXe.disabled = true;
-}
-function dXe() {
-  let x = document.getElementById("dongxe").value;
-  let phienBan = document.getElementById("phienban");
-  let pBan = [];
-  if (x != "Chọn dòng xe") {
-    phienBan.disabled = false;
-    if (x == "Fadil" || x == "VF3" || x == "VF4") {
-      phienBan.innerHTML = [];
-      pBan = ["Cao cấp", "Nâng cao", "Tiêu chuẩn", "Plus"];
-      pBan.forEach(function (value) {
-        let option = document.createElement("option");
-        option.text = value;
-        option.value = value;
-        phienBan.add(option);
-      });
-    }
-    if (x == "Allex" || x == "Allion" || x == "Alphard") {
-      phienBan.innerHTML = [];
-      pBan = ["Khác", "VT3", "3.0 Sedan (FWD)"];
-      pBan.forEach(function (value) {
-        let option = document.createElement("option");
-        option.text = value;
-        option.value = value;
-        phienBan.add(option);
-      });
-    }
-    if (x == "Artic" || x == "Capri" || x == "Cargo") {
-      phienBan.innerHTML = [];
-      pBan = ["Khác", "1.0 Coupe", "1.4 Van (FWD)"];
-      pBan.forEach(function (value) {
-        let option = document.createElement("option");
-        option.text = value;
-        option.value = value;
-        phienBan.add(option);
-      });
-    }
-    if (x == "1600" || x == "1800" || x == "2000") {
-      phienBan.innerHTML = [];
-      pBan = ["Khác", "VT 300"];
-      pBan.forEach(function (value) {
-        let option = document.createElement("option");
-        option.text = value;
-        option.value = value;
-        phienBan.add(option);
-      });
-    }
+  } else {
+    getVehicles.innerHTML = " ";
+    let firstOption = document.createElement("option");
+    firstOption.text = "Chọn dòng xe";
+    getVehicles.add(firstOption);
+    //turn off option
+    getVehicles.disabled = true;
   }
 }
 
-let nsx = document.getElementById("nsx");
+//Set option for Version Select
+let getCarVersion = document.getElementById("carVersion");
+const vinfastVersion = ["Cao cấp", "Nâng cao", "Tiêu chuẩn", "Plus"];
+const toyotaVersion = ["Khác", "VT3", "3.0 Sedan (FWD)"];
+const fordVersion = ["Khác", "1.0 Coupe", "1.4 Van (FWD)"];
+const bmwVersion = ["Khác", "VT 300"];
+const printfOptionVersion = (arr) => {
+  // làm rỗng option
+  getCarVersion.innerHTML = " ";
+  // create 1 option
+  let firstOption = document.createElement("option");
+  firstOption.text = "Chọn phiên bản xe";
+  getCarVersion.add(firstOption);
+  // push option được chọn
+  arr.forEach(function (value) {
+    let option = document.createElement("option");
+    option.text = value;
+    option.value = value;
+    getCarVersion.add(option);
+  });
+};
+function vehicles(value) {
+  if (value != "Chọn dòng xe") {
+    getCarVersion.disabled = false;
+    if (value == "Fadil" || value == "VF3" || value == "VF4") {
+      printfOptionVersion(vinfastVersion);
+    }
+    if (x == "Allex" || x == "Allion" || x == "Alphard") {
+      printfOptionVersion(toyotaVersion);
+    }
+    if (x == "Artic" || x == "Capri" || x == "Cargo") {
+      printfOptionVersion(fordVersion);
+    }
+    if (x == "1600" || x == "1800" || x == "2000") {
+      printfOptionVersion(bmwVersion);
+    }
+  } else {
+    getCarVersion.innerHTML = " ";
+    let firstOption = document.createElement("option");
+    firstOption.text = "Chọn phiên bản xe";
+    getCarVersion.add(firstOption);
+    getCarVersion.disabled = true;
+  }
+}
+
+//set option for Year Select
+let yearProduce = document.getElementById("year");
 let value = [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024];
 value.forEach(function (value) {
   let option = document.createElement("option");
   option.text = value;
   option.value = value;
-  nsx.add(option);
+  yearProduce.add(option);
 });
 
 function button() {
-  let x = document.getElementById("hangxe").value;
-  let y = document.getElementById("dongxe").value;
-  let z = document.getElementById("phienban").value;
-  let t = document.getElementById("kilometer").value;
-  let u = document.getElementById("phone").value;
+  let Vehicles = document.getElementById("vehicles").value;
+  let Version = document.getElementById("carVersion").value;
+  let Year = document.getElementById("year").value;
+  let Kilometer = document.getElementById("kilometer").value;
+  let Phone = document.getElementById("phone").value;
   let btn = document.getElementById("btn");
-  if (x != " " && y != " " && z != " " && t != " " && u > 10**9 ) {
+  if (
+    Vehicles != " " &&
+    Version != " " &&
+    Year != " " &&
+    Kilometer != " " &&
+    Phone > 10 ** 8
+  ) {
     btn.disabled = false;
-  } else return true;
+  } else btn.disabled = true;;
 }
