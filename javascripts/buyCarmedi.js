f4140cd337a31f62835f306cb0f1bc495d8851d8
phuocnguyen10051307 <phuocmobi4321@gmail.com>
2024-04-15 19:13:10 +0700

    Message describing the changes

---
+++ javascripts/buyCarmedi.js
@@ -0,0 +1,221 @@
+// const arr = [
+//   {
+//     name: "Hãng xe, dòng xe",
+//     icon: '<i class="bi bi-building-check"></i>',
+//     down: '<i class="bi bi-chevron-down"></i>',
+//   },
+//   {
+//     name: "Địa điểm",
+//     icon: '<i class="bi bi-geo-alt"></i>',
+//     down: '<i class="bi bi-chevron-down"></i>',
+//   },
+//   {
+//     name: "Giá",
+//     icon: '<i class="bi bi-cash"></i>',
+//     down: '<i class="bi bi-chevron-down"></i>',
+//   },
+//   {
+//     name: "Năm sản xuất",
+//     icon: '<i class="bi bi-calendar-event-fill"></i>',
+//     down: '<i class="bi bi-chevron-down"></i>',
+//   },
+//   {
+//     name: "Số Km",
+//     icon: '<i class="bi bi-speedometer"></i>',
+//     down: '<i class="bi bi-chevron-down"></i>',
+//   },
+//   {
+//     name: "Kiểu dáng",
+//     icon: '<i class="bi bi-car-front-fill"></i>',
+//     down: '<i class="bi bi-chevron-down"></i>',
+//   },
+//   {
+//     name: "Nhiên liệu",
+//     icon: '<i class="bi bi-fuel-pump"></i>',
+//     down: '<i class="bi bi-chevron-down"></i>',
+//   },
+//   {
+//     name: "Hộp số",
+//     icon: '<i class="bi bi-bezier2"></i>',
+//     down: '<i class="bi bi-chevron-down"></i>',
+//   },
+//   {
+//     name: "Màu sắc",
+//     icon: '<i class="bi bi-palette"></i>',
+//     down: '<i class="bi bi-chevron-down"></i>',
+//   },
+//   {
+//     name: "Số Chỗ ngồi",
+//     icon: '<i class="bi bi-usb-plug"></i>',
+//     down: '<i class="bi bi-chevron-down"></i>',
+//   },
+//   {
+//     name: "Đăng bởi",
+//     icon: '<i class="bi bi-person-circle"></i>',
+//     down: '<i class="bi bi-chevron-down"></i>',
+//   },
+// ];
+// let liTag = "";
+// for (let i = 0; i < arr.length; i++) {
+//   liTag += `<li class="list-group-item"><span class='ab'>${arr[i].icon}</span> ${arr[i].name}  <span class='abc'>${arr[i].down}</span></li>`;
+// }
+// document.getElementById("li-tag").innerHTML = liTag;
+const showInfor = [
+  {
+    img: "./assets/car1.jpg",
+    tittle:
+      "2022 MG MG 5 STD (Tiêu chuẩn)- TRUNG TÂM MUA BÁN XE CŨ MG CHÍNH HÃNG",
+    year: "2022",
+    km: "20000",
+    fuel: "Xăng",
+    gear: "Tự động",
+    icon1: '<i class="bi bi-calendar-event-fill"></i>',
+    icon2: '<i class="bi bi-speedometer"></i>',
+    icon3: '<i class="bi bi-fuel-pump"></i>',
+    icon4: '<i class="bi bi-bezier2"></i>',
+    icon5: '<i class="bi bi-geo-alt"></i>',
+    money: "425 Triệu",
+    address: "Hồ Chí Minh - quận 6",
+  },
+  {
+    img: "./assets/car1.jpg",
+    tittle:
+      "2022 MG MG 5 STD (Tiêu chuẩn)- TRUNG TÂM MUA BÁN XE CŨ MG CHÍNH HÃNG",
+    year: "2022",
+    km: "20000",
+    fuel: "Xăng",
+    gear: "Tự động",
+    icon1: '<i class="bi bi-calendar-event-fill"></i>',
+    icon2: '<i class="bi bi-speedometer"></i>',
+    icon3: '<i class="bi bi-fuel-pump"></i>',
+    icon4: '<i class="bi bi-bezier2"></i>',
+    icon5: '<i class="bi bi-geo-alt"></i>',
+    money: "425 Triệu",
+    address: "Hồ Chí Minh - quận 6",
+  },
+  {
+    img: "./assets/car1.jpg",
+    tittle:
+      "2022 MG MG 5 STD (Tiêu chuẩn)- TRUNG TÂM MUA BÁN XE CŨ MG CHÍNH HÃNG",
+    year: "2022",
+    km: "20000",
+    fuel: "Xăng",
+    gear: "Tự động",
+    icon1: '<i class="bi bi-calendar-event-fill"></i>',
+    icon2: '<i class="bi bi-speedometer"></i>',
+    icon3: '<i class="bi bi-fuel-pump"></i>',
+    icon4: '<i class="bi bi-bezier2"></i>',
+    icon5: '<i class="bi bi-geo-alt"></i>',
+    money: "425 Triệu",
+    address: "Hồ Chí Minh - quận 6",
+  },
+  {
+    img: "./assets/car1.jpg",
+    tittle:
+      "2022 MG MG 5 STD (Tiêu chuẩn)- TRUNG TÂM MUA BÁN XE CŨ MG CHÍNH HÃNG",
+    year: "2022",
+    km: "20000",
+    fuel: "Xăng",
+    gear: "Tự động",
+    icon1: '<i class="bi bi-calendar-event-fill"></i>',
+    icon2: '<i class="bi bi-speedometer"></i>',
+    icon3: '<i class="bi bi-fuel-pump"></i>',
+    icon4: '<i class="bi bi-bezier2"></i>',
+    icon5: '<i class="bi bi-geo-alt"></i>',
+    money: "425 Triệu",
+    address: "Hồ Chí Minh - quận 6",
+  },
+  {
+    img: "./assets/car1.jpg",
+    tittle:
+      "2022 MG MG 5 STD (Tiêu chuẩn)- TRUNG TÂM MUA BÁN XE CŨ MG CHÍNH HÃNG",
+    year: "2022",
+    km: "20000",
+    fuel: "Xăng",
+    gear: "Tự động",
+    icon1: '<i class="bi bi-calendar-event-fill"></i>',
+    icon2: '<i class="bi bi-speedometer"></i>',
+    icon3: '<i class="bi bi-fuel-pump"></i>',
+    icon4: '<i class="bi bi-bezier2"></i>',
+    icon5: '<i class="bi bi-geo-alt"></i>',
+    money: "425 Triệu",
+    address: "Hồ Chí Minh - quận 6",
+  },
+  {
+    img: "./assets/car1.jpg",
+    tittle:
+      "2022 MG MG 5 STD (Tiêu chuẩn)- TRUNG TÂM MUA BÁN XE CŨ MG CHÍNH HÃNG",
+    year: "2022",
+    km: "20000",
+    fuel: "Xăng",
+    gear: "Tự động",
+    icon1: '<i class="bi bi-calendar-event-fill"></i>',
+    icon2: '<i class="bi bi-speedometer"></i>',
+    icon3: '<i class="bi bi-fuel-pump"></i>',
+    icon4: '<i class="bi bi-bezier2"></i>',
+    icon5: '<i class="bi bi-geo-alt"></i>',
+    money: "425 Triệu",
+    address: "Hồ Chí Minh - quận 6",
+  },
+];
+
+// let perPage = 6;
+// let currentPage =1;
+// let start = 0;
+// let end =perPage;
+// let divCard=''
+// function renderProduct() {
+//   const content = showInfor.map((item,index)=>{
+//     if(index>= start && index<end) {
+//       divCard += `
+//         <div class="card" >
+//           <img src="${showInfor[i].img}" class="card-img-top">
+//           <div class="card-body">
+//             <h5 class="card-title">${showInfor[i].tittle}</h5>
+//             <p class="card-text">${showInfor[i].icon1} ${showInfor[i].year}</p>
+//             <p class="card-text">${showInfor[i].icon2} ${showInfor[i].km} km</p>
+//             <p class="card-text">${showInfor[i].icon3} ${showInfor[i].fuel}</p>
+//             <p class="card-text">${showInfor[i].icon4} ${showInfor[i].gear}</p>
+//           </div>
+//           <div class="card-footer">
+//             <p class="card-text" style="font-size:20px; color:red;">${showInfor[i].money}</p>
+//             <p class="card-text" style="font-size:12px;">${showInfor[i].icon5} ${showInfor[i].address}</p>
+//           </div>
+//         </div>`;
+//     }
+//     document.getElementById("show").innerHTML = divCard;
+//   })
+
+// }
+// renderProduct();
+
+let divCard = "";
+for (let i = 0; i < showInfor.length; i++) {
+  divCard += `
+  <div class="card" >
+    <img src="${showInfor[i].img}" class="card-img-top">
+    <div class="card-body">
+      <h5 class="card-title">${showInfor[i].tittle}</h5>
+      <p class="card-text">${showInfor[i].icon1} ${showInfor[i].year}</p>
+      <p class="card-text">${showInfor[i].icon2} ${showInfor[i].km} km</p>
+      <p class="card-text">${showInfor[i].icon3} ${showInfor[i].fuel}</p>
+      <p class="card-text">${showInfor[i].icon4} ${showInfor[i].gear}</p>
+    </div>
+    <div class="card-footer">
+      <p class="card-text" style="font-size:20px; color:red;">${showInfor[i].money}</p>
+      <p class="card-text" style="font-size:12px;">${showInfor[i].icon5} ${showInfor[i].address}</p>
+    </div>
+  </div>`;
+}
+document.getElementById("show").innerHTML = divCard;
+
+var button = document.getElementById("bt3");
+var content = document.getElementById("click3");
+
+button.addEventListener("click", function () {
+  if (content.style.display === "none") {
+    content.style.display = "block";
+  } else {
+    content.style.display = "none";
+  }
+});
